45e2fa036bb14fbf6ab7c1f8d85437439d928a5b
mochfamir <faishalkings@gmail.com>
2020-05-11 16:39:03 +0700

    temp(App): fix style

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
 import React from 'react';
 import './App.css';
 
+import { Container } from './components/container/container';
+
 function App() {
-  return <h1>covid19</h1>;
+  const data: string = 'hoho';
+  return (
+    <div className="app">
+      <h1>COVID-19 Overview</h1>
+      <Container data={data} />
+    </div>
+  );
 }
 
 export default App;
